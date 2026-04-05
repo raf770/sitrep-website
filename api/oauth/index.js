db3f2814354efa49f82b607977b80dec6da56197
raf770 <parienteraphael@gmail.com>
2026-04-05 09:15:01 +0200

    Update index.js

--- api/oauth/index.js
+++ api/oauth/index.js
@@ -3,7 +3,7 @@ const CLIENT_ID = 'Ov23liARAoLHgeuGCiYp';
 export default function handler(req, res) {
   const params = new URLSearchParams({
     client_id: CLIENT_ID,
-    redirect_uri: 'https://sitrep-proxy.vercel.app/api/oauth/callback',
+    redirect_uri: 'https://sitrep-website.vercel.app/api/oauth/callback',
     scope: 'repo',
     state: Math.random().toString(36).substr(2)
   });
